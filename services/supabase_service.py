762e7c1c5236d12ce8b4eef288347f26f2647c8d
xone-alfred <paul@brainboxai.com.au>
2026-06-26 13:08:46 +1000

    Implement Supabase client initialization function

---
+++ services/supabase_service.py
@@ -0,0 +1,8 @@
+import os
+from supabase import create_client, Client
+
+
+def get_supabase() -> Client:
+    url = os.environ["SUPABASE_URL"]
+    key = os.environ["SUPABASE_SERVICE_ROLE_KEY"]
+    return create_client(url, key)
